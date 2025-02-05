6082a1228d68efbdb379dffef17a3b59664ab315
Alshahriar78 <alshahriar78@gmail.com>
2025-02-05 15:22:38 +0600

    update

--- app/config/config.js
+++ app/config/config.js
@@ -1,13 +1,13 @@
 export const PORT=5060
-export const DATABASE='mongodb+srv://alshahriar78:nsu5836nsu@cluster0.n16jq.mongodb.net/taskmanager'
+export const DATABASE=''
 export const JWT_KEY='ABC12341241234'
 export const JWT_EXPIRE_TIME=30*24*60*60
 
-export const EMAIL_HOST="mail.teamrabbil.com"
+export const EMAIL_HOST="mail.alsharriar.com"
 export const EMAIL_PORT=25
 export const EMAIL_SECURITY=false
-export const EMAIL_USER="info@teamrabbil.com"
-export const EMAIL_PASS="~sR4[bhaC[Qs"
+export const EMAIL_USER="info@al......com"
+export const EMAIL_PASS="....."
 export const WEB_CACHE=false
 export const MAX_JSON_SIZE="10MB"
 export const URL_ENCODE=true
